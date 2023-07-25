c03de54eaa9ba2080d80948bb210c46a67c231bb
Ankur <ankursaxena2002@gmail.com>
2023-07-25 22:18:16 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+// import Card  from './component/Card/Card';
+import Grid from './component/Grid/Grid';
+
+
+
+
+function App (){
+  return (
+   <>
+
+   <Grid numberofCards={9}/>
+   </>
+  )
+}
+
+export default App
